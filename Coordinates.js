6db6168823cbe980128caee5b4daf066dff09357
KillerBLS <dm.mironov01@gmail.com>
2015-02-05 12:19:29 +0300

    Create Coordinates.js

---
+++ Coordinates.js
@@ -0,0 +1,7 @@
+function modTick()
+{
+var px = getPlayerX();
+var py = getPlayerY();
+var pz = getPlayerZ();
+ModPE.showTipMessage("\n\n\n" + "X: " + parseInt(px) + " Y: " + parseInt(py) + " Z: " + parseInt(pz));
+}
