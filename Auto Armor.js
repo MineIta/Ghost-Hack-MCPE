55ea29d6b0942cfd282617935ad36a1cb1238811
KillerBLS <dm.mironov01@gmail.com>
2015-02-27 15:24:19 +0300

    Create Auto Armor.js

---
+++ Auto Armor.js
@@ -0,0 +1,82 @@
+function useItem(x, y, z, i, b, itemid, blockid, side){
+               if (i==310&& Player.getArmorSlot(0)==0){
+                    Player.setArmorSlot(0, 310);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==311&& Player.getArmorSlot(1) == 0){
+                    Player.setArmorSlot(1, 311);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==312&& Player.getArmorSlot(2) == 0){
+                    Player.setArmorSlot(2, 312);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==313&& Player.getArmorSlot(3) == 0){
+                    Player.setArmorSlot(3, 313);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==306&& Player.getArmorSlot(0) == 0){
+                    Player.setArmorSlot(0, 306);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==307&& Player.getArmorSlot(1) == 0){
+                    Player.setArmorSlot(1, 307);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==308&& Player.getArmorSlot(2) == 0){
+                    Player.setArmorSlot(2, 308);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==309&& Player.getArmorSlot(3) == 0){
+                    Player.setArmorSlot(3, 309);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+              if (i==302&& Player.getArmorSlot(0) == 0){
+                    Player.setArmorSlot(0, 302);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==303&&Player.getArmorSlot(1)==0){
+                    Player.setArmorSlot(1, 303);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==304&&Player.getArmorSlot(2)==0){
+                    Player.setArmorSlot(2, 304);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==305&&Player.getArmorSlot(3)==0){
+                    Player.setArmorSlot(3, 305);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==314&&Player.getArmorSlot(0) == 0){
+                    Player.setArmorSlot(0, 314);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==315&&Player.getArmorSlot(1) == 0){
+                    Player.setArmorSlot(1, 315);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==316&&Player.getArmorSlot(2) == 0){
+                    Player.setArmorSlot(2, 316);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+                if (i==317&&Player.getArmorSlot(3) == 0){
+                    Player.setArmorSlot(3, 317);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==298&&Player.getArmorSlot(0)==0){
+                    Player.setArmorSlot(0, 298);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==299&&Player.getArmorSlot(1)==0){
+                    Player.setArmorSlot(1, 299);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==300&&Player.getArmorSlot(2) == 0){
+                    Player.setArmorSlot(2, 300);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+               if (i==301&&Player.getArmorSlot(3) == 0){
+                    Player.setArmorSlot(3, 301);
+                    Entity.setCarriedItem(getPlayerEnt(), 0);
+}
+}
